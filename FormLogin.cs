ba34e1f3f00f8b76bd8ed3310ffad2b9e2ae9127
fabian-pratama <fabiuss039@gmail.com>
2025-11-08 11:35:44 +0700

    Add files via upload

---
+++ FormLogin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManajemenToko
+{
+    using static GlobalVariable;
+
+
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        public static DateTime WaktuLogin;
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = Koneksi.GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(
+                "SELECT Id, Username FROM [User] WHERE Username = @user AND Password = @pass", conn);
+                cmd.Parameters.AddWithValue("@user", txtUsername.Text.Trim());
+                cmd.Parameters.AddWithValue("@pass", txtPassword.Text.Trim());
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    LoggedInUserId = Convert.ToInt32(reader["Id"]);
+                    LoggedInUsername = reader["Username"].ToString();
+                    FormMenuUtama formMenu = new FormMenuUtama();
+                    this.Hide();
+                    formMenu.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Login gagal. Cek username dan password.");
+                }
+            }
+
+            WaktuLogin = DateTime.Now;
+        }
+    }
+}
---
+++ FormMenuUtama.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManajemenToko
+{
+    using static GlobalVariable;
+    public partial class FormMenuUtama : Form
+    {
+
+        public FormMenuUtama()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMenuUtama_Load(object sender, EventArgs e)
+        {
+            lblHalo.Text = $"Halo, {LoggedInUsername}!";
+            lblWaktuLogin.Text = $"Login pada: {FormLogin.WaktuLogin:dd MMM yyyy HH:mm:ss}";
+            picProfile.Image = Properties.Resources.default_user;
+            picProfile.SizeMode = PictureBoxSizeMode.Zoom;
+
+        }
+
+        private void btnProduk_Click(object sender, EventArgs e)
+        {
+            FormProdukUtama form = new FormProdukUtama();
+            form.ShowDialog();
+        }
+
+        private void btnTransaksi_Click(object sender, EventArgs e)
+        {
+            FormTransaksiPenjualan form = new FormTransaksiPenjualan();
+            form.ShowDialog();
+        }
+
+        private void btnLaporan_Click(object sender, EventArgs e)
+        {
+            FormLaporan form = new FormLaporan();
+            form.ShowDialog();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            // Konfirmasi logout
+            DialogResult result = MessageBox.Show("Apakah kamu yakin ingin logout?",
+                "Konfirmasi Logout", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                LoggedInUserId = 0;
+                LoggedInUsername = null;
+
+                this.Hide();
+                FormLogin formLogin = new FormLogin();
+                formLogin.ShowDialog();
+                this.Close();
+            }
+        }
+
+
+
+        private void picProfile_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
