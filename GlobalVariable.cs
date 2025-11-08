d6cf33168dd0e74b0892e61b841c12b3a945ad10
fabian-pratama <fabiuss039@gmail.com>
2025-11-08 11:36:17 +0700

    Add files via upload

---
+++ GlobalVariable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManajemenToko
+{
+    internal class GlobalVariable
+    {
+        public static int LoggedInUserId;
+        public static string LoggedInUsername;
+    }
+}
